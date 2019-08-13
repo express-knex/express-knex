19c30f62e0302f92757d275115d47332c529f436
deksden <deksden@deksden.com>
2019-08-14 02:03:58 +0700

    feat(validator): working validator

--- packages/validator/src/index.js
+++ packages/validator/src/index.js
@@ -1,9 +1,39 @@
+import { body, validationResult, matchedData } from 'express-validator'
+
 export default (app) => {
-  const Validator = {
-    validate: (Model) => (req, res, next) => {
-      next()
-    },
-    check: (model, part)
+  if (!app.errors) {
+    throw Error('Controller package: expect app.errors to be mounted with proper error classes')
   }
+  if (!app.errors.ServerInvalidParams) {
+    throw Error('Controller package can not find expected error classes at app.errors')
+  }
+
+  const Validator = {
+    validatorFromModel: (Model) => {
+      const validations = []
+      Model.props.map((prop) => {
+        if (prop.type === 'text') {
+          validations.push(body[prop.name].isString().withMessage(`${Model.name}.${prop.name} should be string`))
+        } else if (prop.type === 'id') {
+          validations.push(body[prop.name].optional().isString().withMessage(`${Model.name}.${prop.name} should be string UUID`))
+        }
+      })
+      return validations
+    },
+    applyValidationsToReq: (validations, req) => {
+      return Promise.all(validations.map((validation) => validation.run(req)))
+        .then(() => {
+          req.validationErrors = validationResult(req)
+          if (!req.validationErrors.isEmpty()) {
+            // TODO: add logging error
+            throw new app.errors.ServerInvalidParams(req.validationErrors.mapped())
+          }
+          req.matchedData = matchedData(req)
+          return req
+        })
+        .catch((err) => { throw err })
+    }
+  }
+
   return Validator
 }
--- packages/validator/test/index.js
+++ packages/validator/test/index.js
@@ -1,16 +1,20 @@
 import chai, { expect } from 'chai'
 import dirtyChai from 'dirty-chai'
 import Validator from '../src'
+import Errors from '@express-knex/error'
 chai.use(dirtyChai)
 
 describe('Controller package test', () => {
   it('Property test', () => {
     const app = { env: {} }
+    app.errors = Errors(app)
     app.validator = Validator(app)
     expect(app.validator).to.not.undefined()
     expect(app.validator).to.not.null()
     expect(app.validator).to.include.keys(
-      'checkModel')
-    expect(app.validator.checkModel).to.be.a('function')
+      'validatorFromModel',
+      'applyValidationsToReq')
+    expect(app.validator.validatorFromModel).to.be.a('function')
+    expect(app.validator.applyValidationsToReq).to.be.a('function')
   })
 })
