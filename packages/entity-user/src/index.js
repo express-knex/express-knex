ef5098814c43c256e2b1bd895a7ff7fedcb15f74
deksden <deksden@deksden.com>
2019-10-23 21:49:53 +0700

    feat(entity-user): use deps

--- packages/entity-user/src/index.js
+++ packages/entity-user/src/index.js
@@ -15,17 +15,35 @@ const packageName = 'Entity-user package'
 */
 
 export default module.exports = (app) => {
-  if (!app.storage) {
-    throw Error(`${packageName}: expect app.storage to be mounted`)
+  // guard empty app object
+  if (!app.meta) {
+    app.meta = {}
+  }
+  if (!app.meta.modules) {
+    app.meta.modules = []
+  }
+  if (!app.meta.actions) {
+    app.meta.actions = []
   }
 
-  if (!app.storage.processBeforeSaveToStorage || !app.storage.processAfterLoadFromStorage) {
-    throw Error(`${packageName}: expect app.storage to have mounted functions`)
-  }
+  // define dependencies:
+  app.meta.modules.push({
+    module: packageName,
+    dependency: ['storage', 'storage.processBeforeSaveToStorage', 'storage.processAfterLoadFromStorage']
+  })
 
-  if (!app.controller || !app.controller.CrudActions) {
-    throw Error(`${packageName}: app.controller.CrudActions should be initialized`)
-  }
+  app.meta.modules.push({
+    module: packageName,
+    dependency: 'controller.CrudActions'
+  })
+
+  app.meta.modules.push({
+    module: packageName,
+    dependency: [
+      'errors', 'errors.ServerInvalidUsernamePassword',
+      'app.errors.ServerNotAllowed', 'app.errors.ServerGenericError'
+    ]
+  })
 
   const Model = {
     name: 'User',
@@ -105,7 +123,7 @@ export default module.exports = (app) => {
   if (!app.meta) {
     app.meta = {}
   }
-  app.meta.actions = app.actions.concat(Model.actions)
+  app.meta.actions = app.meta.actions.concat(Model.actions)
 
   return Model
 }
